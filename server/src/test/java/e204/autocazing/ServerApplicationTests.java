af7b442e92ca7a530db56366e460648b30364525
kimmin <kimmin5714@gmail.com>
2024-04-23 16:24:26 +0900

    init

---
+++ server/src/test/java/e204/autocazing/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package e204.autocazing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
