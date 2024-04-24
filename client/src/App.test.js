7b0dd2653e2b7baf9555e86d92993ac264755897
pmhmist <pmhmist@naver.com>
2024-04-24 18:01:53 +0900

    fix: 에러 수정

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-=======
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
@@ -14,5 +5,4 @@ test("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
->>>>>>> eea29e5672105d70fe05cbda547ccfa3c0034cec
 });
