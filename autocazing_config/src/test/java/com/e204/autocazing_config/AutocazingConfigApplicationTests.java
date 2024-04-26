912b0e5fa0f42a392fe1f45b54462185331e1289
김영후 <eagle7449@naver.com>
2024-04-26 17:44:51 +0900

    Feat: config server 프로젝트 생성

---
+++ autocazing_config/src/test/java/com/e204/autocazing_config/AutocazingConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e204.autocazing_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocazingConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
