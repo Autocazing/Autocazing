46d12571b52ea1edaa27b4c4888774be3379c03e
kimmin <kimmin5714@gmail.com>
2024-04-26 17:44:33 +0900

    Feat: api gateway server init

---
+++ autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/AutocazingApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.e204.autocazing_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocazingApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocazingApigatewayApplication.class, args);
+	}
+
+}
