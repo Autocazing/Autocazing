ed0d72eaf7b83a794d1b91065ec2762343b977d7
진성민 <sengmin14@naver.com>
2024-04-26 11:01:31 +0900

    feat : tailwind css적용

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 
 import Test from "./pages/Test";
-
 function App() {
     return (
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    <>
-                        {/* <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
-                        <Test />
-                    </>
-                }
-            />
-        </Routes>
+        <div>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            {/* <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
+                            <Test />
+                        </>
+                    }
+                />
+            </Routes>
+            {/* <div className="text-3xl font-bold underline">hello</div> */}
+        </div>
     );
 }
 
