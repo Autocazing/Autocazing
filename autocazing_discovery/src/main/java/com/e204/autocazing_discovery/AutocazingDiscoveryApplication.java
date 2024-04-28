d9317564679ec08aa08d499f78c5fe0c20eff4d4
kimmin <kimmin5714@gmail.com>
2024-04-29 00:48:58 +0900

    Feat: discovery server init

---
+++ autocazing_discovery/src/main/java/com/e204/autocazing_discovery/AutocazingDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.e204.autocazing_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocazingDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocazingDiscoveryApplication.class, args);
+	}
+
+}
