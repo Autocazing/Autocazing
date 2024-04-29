8c52a1db933f1690e34a7d3fd198f5e3282a331c
icefalls <pmhmist@naver.com>
2024-04-29 21:56:55 +0900

    fix: 젠킨스 에러 수정

--- client/src/components/ChartOne.jsx
+++ client/src/components/ChartOne.jsx
@@ -128,13 +128,6 @@ const ChartOne = () => {
         ],
     });
 
-    const handleReset = () => {
-        setState((prevState) => ({
-            ...prevState,
-        }));
-    };
-    // handleReset;
-
     return (
         <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
             <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 
