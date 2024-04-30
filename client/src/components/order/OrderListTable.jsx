90f2405289e72ba7f9de3b767f0fb08c39dec683
pmhmist <pmhmist@naver.com>
2024-04-30 14:59:11 +0900

    feat : 장바구니 게시한 레이아웃 구현

--- client/src/components/order/OrderListTable.jsx
+++ client/src/components/order/OrderListTable.jsx
@@ -1,41 +1,28 @@
-import Order from "../../pages/order/Order";
+import modifyIcon from "../../images/orderlist/modify.svg";
+import deleteIcon from "../../images/orderlist/delete.svg";
 
 const OrderList = [
     // 테스트용
     {
-        name: "Google",
-        visitors: 3.5,
-        revenues: "5,768",
-        sales: 590,
-        conversion: 4.8,
+        name: "우유",
+        amount: 1,
+        price: "2000",
+        company: "동민상사",
+        ordertime: 1,
     },
     {
-        name: "Twitter",
-        visitors: 2.2,
-        revenues: "4,635",
-        sales: 467,
-        conversion: 4.3,
+        name: "원두",
+        amount: 2,
+        price: "8000",
+        company: "동민상사",
+        ordertime: 2,
     },
     {
-        name: "Github",
-        visitors: 2.1,
-        revenues: "4,290",
-        sales: 420,
-        conversion: 3.7,
-    },
-    {
-        name: "Vimeo",
-        visitors: 1.5,
-        revenues: "3,580",
-        sales: 389,
-        conversion: 2.5,
-    },
-    {
-        name: "Facebook",
-        visitors: 3.5,
-        revenues: "6,768",
-        sales: 390,
-        conversion: 4.2,
+        name: "연유",
+        amount: 1,
+        price: "9000",
+        company: "민호상사",
+        ordertime: 7,
     },
 ];
 
@@ -43,41 +30,46 @@ const OrderListTable = () => {
     return (
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
             <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-                Top Channels
+                Order List
             </h4>
 
             <div className="flex flex-col">
-                <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+                <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-6">
                     <div className="p-2.5 xl:p-5">
                         <h5 className="text-sm font-medium uppercase xsm:text-base">
-                            Source
+                            재료명
                         </h5>
                     </div>
                     <div className="p-2.5 text-center xl:p-5">
                         <h5 className="text-sm font-medium uppercase xsm:text-base">
-                            Visitors
+                            용량
                         </h5>
                     </div>
                     <div className="p-2.5 text-center xl:p-5">
                         <h5 className="text-sm font-medium uppercase xsm:text-base">
-                            Revenues
+                            가격(원)
                         </h5>
                     </div>
                     <div className="hidden p-2.5 text-center sm:block xl:p-5">
                         <h5 className="text-sm font-medium uppercase xsm:text-base">
-                            Sales
+                            업체명
                         </h5>
                     </div>
                     <div className="hidden p-2.5 text-center sm:block xl:p-5">
                         <h5 className="text-sm font-medium uppercase xsm:text-base">
-                            Conversion
+                            배송소요기간
+                        </h5>
+                    </div>
+                    <div className="hidden p-2.5 text-center sm:block xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            수정/삭제
                         </h5>
                     </div>
                 </div>
 
                 {OrderList.map((order, key) => (
                     <div
-                        className={`grid grid-cols-3 sm:grid-cols-5 ${
+                        className={`grid grid-cols-3 sm:grid-cols-6 ${
                             key === OrderList.length - 1
                                 ? ""
                                 : "border-b border-stroke dark:border-strokedark"
@@ -96,22 +88,34 @@ const OrderListTable = () => {
 
                         <div className="flex items-center justify-center p-2.5 xl:p-5">
                             <p className="text-black dark:text-white">
-                                {order.visitors}K
+                                {order.amount}
                             </p>
                         </div>
 
                         <div className="flex items-center justify-center p-2.5 xl:p-5">
-                            <p className="text-meta-3">${order.revenues}</p>
-                        </div>
-
-                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
                             <p className="text-black dark:text-white">
-                                {order.sales}
+                                {order.price}
                             </p>
                         </div>
 
                         <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                            <p className="text-meta-5">{order.conversion}%</p>
+                            <p className="text-black dark:text-white">
+                                {order.company}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {order.ordertime}
+                            </p>
+                        </div>
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <button className="mr-2">
+                                <img src={modifyIcon} alt="Modify" />
+                            </button>
+                            <button>
+                                <img src={deleteIcon} alt="delete" />
+                            </button>
                         </div>
                     </div>
                 ))}
