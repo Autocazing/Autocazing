41918a369964d88452dcd94c2355291334ce90e5
kimmin <kimmin5714@gmail.com>
2024-04-30 11:50:04 +0900

    Feat: 라우팅 확인

--- autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/AutocazingApigatewayApplication.java
+++ autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/AutocazingApigatewayApplication.java
@@ -2,8 +2,10 @@ package com.e204.autocazing_apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AutocazingApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutocazingApigatewayApplication.class, args);
---
+++ autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.e204.autocazing_apigateway;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+			.route(r -> r.path("/test1/**")
+				.filters(f -> f.addRequestHeader("first-request","first-request-header2")
+					.addResponseHeader("first-response","first-response-header2"))
+				.uri("http://localhost:8082"))
+			.route(r -> r.path("/test2/**")
+				.filters(f -> f.addRequestHeader("second-request","second-request-header2")
+					.addResponseHeader("second-response","second-response-header2"))
+				.uri("http://localhost:8083"))
+			.build();
+	}
+}
