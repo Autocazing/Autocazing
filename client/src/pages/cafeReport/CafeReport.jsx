7c9fd3286435f6e4a4c767dc1df4fd3b749ddb7b
moonwlgh <moonwlgh@gmail.com>
2024-05-02 10:13:23 +0900

    Init: fullcalendar 라이브러리 설치

--- client/src/pages/cafeReport/CafeReport.jsx
+++ client/src/pages/cafeReport/CafeReport.jsx
@@ -1,13 +1,15 @@
 const CafeReport = () => {
     return (
-        <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-            <h2 className="text-title-md2 font-semibold text-black dark:text-white">
-                리포트
-            </h2>
-            <ol className="flex items-center gap-2">
-                <li>리포트 /</li>
-                <li className="font-bold text-primary">리포트</li>
-            </ol>
+        <div>
+            <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+                <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+                    리포트
+                </h2>
+                <ol className="flex items-center gap-2">
+                    <li>리포트 /</li>
+                    <li className="font-bold text-primary">리포트</li>
+                </ol>
+            </div>
         </div>
     );
 };
