dde99098eb6a662f0f7a30a46fa7bbba34aff3a8
moonwlgh <moonwlgh@gmail.com>
2024-05-02 16:23:44 +0900

    Feat: 리포트 모달창 완료

--- client/src/pages/cafeReport/ReportModal.jsx
+++ client/src/pages/cafeReport/ReportModal.jsx
@@ -11,8 +11,8 @@ const customStyles = {
         left: "0",
     },
     content: {
-        width: "360px",
-        height: "180px",
+        width: "30rem",
+        height: "70%",
         zIndex: "150",
         position: "absolute",
         top: "50%",
@@ -21,7 +21,10 @@ const customStyles = {
         borderRadius: "10px",
         boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
         backgroundColor: "white",
-        justifyContent: "center",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
         overflow: "auto",
     },
 };
@@ -35,10 +38,24 @@ const ReportModal = ({ isOpen, onClose, data }) => {
             contentLabel="Report Details"
             ariaHideApp={false}
         >
-            <h2>{data.date} 리포트</h2>
-            <p>어제: {data.yesterday}</p>
-            <p>예측: {data.predict}</p>
-            <button onClick={onClose}>닫기</button>
+            <div>
+                <div>
+                    <h1 className="text-3xl my-4 font-semibold text-black dark:text-white">
+                        {data.date} 리포트
+                    </h1>
+                </div>
+                <div>
+                    <p>어제: {data.yesterday}</p>
+                    <p>예측: {data.predict}</p>
+                </div>
+            </div>
+
+            <button
+                onClick={onClose}
+                className="mt-auto mx-auto inline-flex items-center justify-center rounded-md bg-primary py-2 px-4 text-center font-medium text-white hover:bg-opacity-90 focus:outline-none"
+            >
+                닫기
+            </button>
         </Modal>
     );
 };
