fcc490279084ac42f11178c8b3588ffea4e1cea1
pmhmist <pmhmist@naver.com>
2024-05-02 09:47:35 +0900

    design : Management 페이지 상단 레이아웃 구성

--- client/src/pages/management/StockManagement.jsx
+++ client/src/pages/management/StockManagement.jsx
@@ -1,5 +1,15 @@
 const StockManagement = () => {
-    return <div>StockManagement</div>;
+    return (
+        <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+            <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+                재고 관리
+            </h2>
+            <ol className="flex items-center gap-2">
+                <li>가게 관리 /</li>
+                <li className="font-bold text-primary">재고 관리</li>
+            </ol>
+        </div>
+    );
 };
 
 export default StockManagement;
