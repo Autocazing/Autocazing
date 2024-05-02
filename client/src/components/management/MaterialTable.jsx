83e07d8e90e2b5a06af2776646d6a14560063d05
pmhmist <pmhmist@naver.com>
2024-05-02 10:22:20 +0900

    feat: 재료관리 페이지 초안

--- client/src/components/management/MaterialTable.jsx
+++ client/src/components/management/MaterialTable.jsx
@@ -1,5 +1,143 @@
+import modifyIcon from "../../images/orderlist/modify.svg";
+import deleteIcon from "../../images/orderlist/delete.svg";
+
+const MaterialList = [
+    // 테스트용
+    {
+        materialname: "서울 우유",
+        price: "2000",
+        amount: "1",
+        minnum: 2,
+        delivertime: 2,
+        companyname: "싸피상사",
+    },
+    {
+        materialname: "연유",
+        price: "4000",
+        amount: "1",
+        minnum: 1,
+        delivertime: 3,
+        companyname: "쿠팡",
+    },
+    {
+        materialname: "원두",
+        price: "10000",
+        amount: "2",
+        minnum: 3,
+        delivertime: 1,
+        companyname: "지호 원두",
+    },
+];
+
 const MaterialTable = () => {
-    return <></>;
+    return (
+        <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+            <div className="flex-col gap-3 flex sm:flex-row sm:items-center sm:justify-between">
+                <h4 className="mb-6 text-xl font-semibold text-black dark:text-white flex-row">
+                    Material List
+                </h4>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    메뉴추가
+                </button>
+            </div>
+
+            <div className="flex flex-col">
+                <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-7">
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            재료명
+                        </h5>
+                    </div>
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            재료 가격(원)
+                        </h5>
+                    </div>
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            용량
+                        </h5>
+                    </div>
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            최소 개수
+                        </h5>
+                    </div>
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            배송소요기간
+                        </h5>
+                    </div>
+                    <div className="p-2.5 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            업체명
+                        </h5>
+                    </div>
+                    <div className="hidden p-2.5 text-center sm:block xl:p-5">
+                        <h5 className="text-sm font-medium uppercase xsm:text-base">
+                            수정/삭제
+                        </h5>
+                    </div>
+                </div>
+
+                {MaterialList.map((material, key) => (
+                    <div
+                        className={`grid grid-cols-3 sm:grid-cols-7 ${
+                            key === MaterialList.length - 1
+                                ? ""
+                                : "border-b border-stroke dark:border-strokedark"
+                        }`}
+                        key={key}
+                    >
+                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.materialname}
+                            </p>
+                        </div>
+
+                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.price}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.amount}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.minnum}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.delivertime}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <p className="text-black dark:text-white">
+                                {material.companyname}
+                            </p>
+                        </div>
+
+                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                            <button className="mr-2">
+                                <img src={modifyIcon} alt="Modify" />
+                            </button>
+                            <button>
+                                <img src={deleteIcon} alt="delete" />
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default MaterialTable;
--- client/src/pages/management/MaterialManagement.jsx
+++ client/src/pages/management/MaterialManagement.jsx
@@ -1,14 +1,19 @@
+import MaterialTable from "../../components/management/MaterialTable";
+
 const MaterialManagement = () => {
     return (
-        <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-            <h2 className="text-title-md2 font-semibold text-black dark:text-white">
-                업체 관리
-            </h2>
-            <ol className="flex items-center gap-2">
-                <li>재료 관리 /</li>
-                <li className="font-bold text-primary">재료 관리</li>
-            </ol>
-        </div>
+        <>
+            <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+                <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+                    재료 관리
+                </h2>
+                <ol className="flex items-center gap-2">
+                    <li>재료 관리 /</li>
+                    <li className="font-bold text-primary">재료 관리</li>
+                </ol>
+            </div>
+            <MaterialTable />
+        </>
     );
 };
 
