e2e62b7f240c6e01aad16d3e97bb7256d1888568
문지호 <moonwlgh@gmail.com>
2024-05-06 20:45:24 +0900

    Feat: 모달 input 완료

--- client/src/pages/management/StockManagementModal.jsx
+++ client/src/pages/management/StockManagementModal.jsx
@@ -29,7 +29,104 @@ const customStyles = {
     },
 };
 const StockManagementModal = ({ isOpen, onClose }) => {
-    return <></>;
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onClose}
+            style={customStyles}
+            contentLabel="Add Company"
+            ariaHideApp={false}
+        >
+            <div className="flex justify-end">
+                <button
+                    onClick={onClose}
+                    style={{
+                        background: "transparent",
+                        border: "none",
+                        cursor: "pointer",
+                    }}
+                    aria-label="Close modal"
+                >
+                    <img
+                        src={closeIcon}
+                        alt="Close"
+                        style={{ width: "24px", height: "24px" }}
+                    />
+                </button>
+            </div>
+            <h1 className="text-3xl my-4 font-semibold text-black dark:text-white">
+                재료추가
+            </h1>
+            <div className="p-6.5">
+                <div className="mb-4.5">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                        품목명
+                    </label>
+                    <select className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary">
+                        <option
+                            value=""
+                            disabled
+                            className="text-body dark:text-bodydark"
+                        >
+                            재고 추가할 품목을 선택해주세요
+                        </option>
+                        <option
+                            value="우유"
+                            className="text-body dark:text-bodydark"
+                        >
+                            우유
+                        </option>
+                        <option
+                            value="원두"
+                            className="text-body dark:text-bodydark"
+                        >
+                            원두
+                        </option>
+                        <option
+                            value="연유"
+                            className="text-body dark:text-bodydark"
+                        >
+                            연유
+                        </option>
+                    </select>
+                </div>
+                <div className="mb-4.5">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                        총량
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="총량 입력"
+                        className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                    />
+                </div>
+                <div className="mb-4.5">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                        유통기한
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="YYYY-MM-DD"
+                        className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                    />
+                </div>
+                <div className="mb-4.5">
+                    <label className="mb-2.5 block text-black dark:text-white">
+                        발주 예정 수량
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="발주 예정 수량 입력"
+                        className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                    />
+                </div>
+
+                <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 ">
+                    추가하기
+                </button>
+            </div>
+        </Modal>
+    );
 };
 
 export default StockManagementModal;
