4c3723aa4bc32e8f7619382c0a55d421dd805481
문지호 <moonwlgh@gmail.com>
2024-05-06 20:08:42 +0900

    Feat: 재고추가 버튼 및 모달 생성

--- client/src/pages/management/StockManagement.jsx
+++ client/src/pages/management/StockManagement.jsx
@@ -1,6 +1,8 @@
 import StockTable from "../../components/management/StockTable";
-
+import StockManagementModal from "./StockManagementModal";
+import { useState } from "react";
 const StockManagement = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return (
         <>
             <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
@@ -13,6 +15,18 @@ const StockManagement = () => {
                 </ol>
             </div>
             <StockTable />
+            <button
+                onClick={() => setModalIsOpen(true)}
+                className="bg-transparent hover:bg-primary text-primary font-semibold hover:text-white py-2 px-4 border border-primary hover:border-transparent rounded"
+            >
+                재고추가
+            </button>
+            {modalIsOpen && (
+                <StockManagementModal
+                    isOpen={modalIsOpen}
+                    onClose={() => setModalIsOpen(false)}
+                />
+            )}
         </>
     );
 };
--- client/src/pages/management/StockManagementModal.jsx
+++ client/src/pages/management/StockManagementModal.jsx
@@ -1,4 +1,34 @@
-const StockManagementModal = () => {
+import Modal from "react-modal";
+import { useState } from "react";
+import closeIcon from "../../images/icon/close.svg";
+const customStyles = {
+    overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.4)",
+        width: "100%",
+        height: "100vh",
+        zIndex: "10",
+        position: "fixed",
+        top: "0",
+        left: "0",
+    },
+    content: {
+        width: "30rem",
+        height: "65%",
+        zIndex: "150",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        borderRadius: "10px",
+        boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+        backgroundColor: "white",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "auto",
+    },
+};
+const StockManagementModal = ({ isOpen, onClose }) => {
     return <></>;
 };
 
