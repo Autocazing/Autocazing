c71bfea02e9a4e18f81b0adeff03cc5cf638dde4
kimmin <kimmin5714@gmail.com>
2024-05-06 16:11:08 +0900

    Fix: cors설정 추가

--- autocazing_auth/src/main/java/com/e204/autocazing_auth/config/security/SecurityConfig.java
+++ autocazing_auth/src/main/java/com/e204/autocazing_auth/config/security/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.e204.autocazing_auth.filter.AuthenticationFilter;
 import com.e204.autocazing_auth.store.service.StoreService;
@@ -32,6 +35,7 @@ public class SecurityConfig {
 
 		http
 			.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+			.cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 적용
 			.authorizeHttpRequests(authorize -> authorize //로그인, 회원가입 api만 권한 없어도 접근 가능
 				.requestMatchers("/api/users/login").permitAll()
 				.requestMatchers("/api/users/register").permitAll()
@@ -60,4 +64,22 @@ public class SecurityConfig {
 	private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
 		return new AuthenticationFilter(authenticationManager, storeService, env);
 	}
+
+	@Bean
+	protected CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("http://localhost:3000");	// CRA로 빌드한 리액트 프로젝트
+		corsConfiguration.addAllowedOrigin("http://localhost:8080");
+		corsConfiguration.addAllowedOrigin("https://k10e204.p.ssafy.io");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.setAllowCredentials(true);	// 응답에 Access-Control-Allow-Credentials 헤더를 true로 설정
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return source;
+	}
+
 }
