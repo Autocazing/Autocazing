1b41a476eb9bffd48cef5f3a814abf6936a43f92
김영후 <eagle7449@naver.com>
2024-05-07 21:40:52 +0900

    Fix: Swagger config 수정

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/config/SwaggerConfig.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/config/SwaggerConfig.java
@@ -14,15 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Server localServer = new Server().url("http://localhost:8080").description("Local HTTP Server");
-        Server httpsServer = new Server().url("https://j10e202.p.ssafy.io").description("Prod HTTPS Server");
+        Server httpsServer = new Server().url("https://k10e204.p.ssafy.io").description("Prod HTTPS Server");
         return new OpenAPI()
                 .servers(Arrays.asList(httpsServer, localServer))
                 .info(apiInfo());
     }
     private Info apiInfo() {
         return new Info()
-                .title("도로로 Swagger")
-                .description("도로로의 API 명세입니다람쥐드래곤.");
+                .title("오토카징 Alert Swagger")
+                .description("오토카징의 Alert server API 명세입니다람쥐드래곤.");
     }
 
 }
