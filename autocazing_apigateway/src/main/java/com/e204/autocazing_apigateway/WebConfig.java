34734a4cf8fcea58c5e2ba9cb50b01e6383dc6b6
김영후 <eagle7449@naver.com>
2024-05-09 00:06:35 +0900

    HOT FIX

--- autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/WebConfig.java
+++ autocazing_apigateway/src/main/java/com/e204/autocazing_apigateway/WebConfig.java
@@ -13,16 +13,16 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://k10e204.p.ssafy.io")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("https://k10e204.p.ssafy.io")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .exposedHeaders("*");
+//    }
 
     @Bean
     public CorsWebFilter corsFilter() {
