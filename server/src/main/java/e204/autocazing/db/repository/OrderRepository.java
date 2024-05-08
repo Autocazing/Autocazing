f8257963ee294db5d87bcc0e9b38d87663a247a9
kimmin <kimmin5714@gmail.com>
2024-05-08 16:06:59 +0900

    Fix: 중간 커밋

--- server/src/main/java/e204/autocazing/db/repository/OrderRepository.java
+++ server/src/main/java/e204/autocazing/db/repository/OrderRepository.java
@@ -11,25 +11,25 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
-
-	@Query(value = "SELECT new map(DATE(o.createdAt) as date, SUM(os.quantity * os.price) as totalSales) "
+	@Query(value = "SELECT new map(DATE(o.createdAt) as date, SUM(os.menuQuantity * os.menuPrice) as totalSales) "
 		+ "FROM OrderEntity o JOIN o.orderSpecific os "
 		+ "WHERE o.createdAt >= :startDate "
 		+ "GROUP BY DATE(o.createdAt) "
-		+ "ORDER BY DATE(o.createdAt)", nativeQuery = true)
+		+ "ORDER BY DATE(o.createdAt)")
 	List<Map<String, Object>> calculateDailySales(LocalDateTime startDate);
 
-	@Query(value = "SELECT new map(WEEK(o.createdAt) as week, YEAR(o.createdAt) as year, SUM(os.quantity * os.price) as totalSales) "
+
+	@Query(value = "SELECT new map(WEEK(o.createdAt) as week, YEAR(o.createdAt) as year, SUM(os.menuQuantity * os.menuPrice) as totalSales) "
 		+"FROM OrderEntity o JOIN o.orderSpecific os "
 		+"WHERE o.createdAt >= :startDate "
 		+"GROUP BY WEEK(o.createdAt), YEAR(o.createdAt) "
-		+"ORDER BY YEAR(o.createdAt), WEEK(o.createdAt)",nativeQuery = true)
+		+"ORDER BY YEAR(o.createdAt), WEEK(o.createdAt)")
 	List<Map<String, Object>> calculateWeekSales(LocalDateTime startDate);
 
-	@Query(value = "SELECT new map(MONTH(o.createdAt) as month, YEAR(o.createdAt) as year, SUM(os.quantity * os.price) as totalSales) "
+	@Query(value = "SELECT new map(MONTH(o.createdAt) as month, YEAR(o.createdAt) as year, SUM(os.menuQuantity * os.menuPrice) as totalSales) "
 		+"FROM OrderEntity o JOIN o.orderSpecific os "
 		+"WHERE o.createdAt >= :startDate "
 		+"GROUP BY MONTH(o.createdAt), YEAR(o.createdAt) "
-		+"ORDER BY YEAR(o.createdAt), MONTH(o.createdAt)", nativeQuery = true)
+		+"ORDER BY YEAR(o.createdAt), MONTH(o.createdAt)")
 	List<Map<String, Object>> calculateMonthSales(LocalDateTime startDate);
 }
