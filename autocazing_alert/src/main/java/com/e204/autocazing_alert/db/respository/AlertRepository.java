5360f2717ca4920d9cbb21a1fd6d6fd96d5b635e
김영후 <eagle7449@naver.com>
2024-05-08 12:13:50 +0900

    Feat: AlertRepository 생성

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/db/respository/AlertRepository.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/db/respository/AlertRepository.java
@@ -1,4 +1,8 @@
 package com.e204.autocazing_alert.db.respository;
 
-public interface AlertRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<AlertRepository, Integer> {
 }
