cf912c9870767eb9a7ee24a7824c0b716f8142c9
kimmin <kimmin5714@gmail.com>
2024-05-08 14:16:53 +0900

    Docs: swagger auth

--- server/src/main/java/e204/autocazing/config/SwaggerConfig.java
+++ server/src/main/java/e204/autocazing/config/SwaggerConfig.java
@@ -1,13 +1,28 @@
 package e204.autocazing.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
 import java.util.Arrays;
 @Configuration
 public class SwaggerConfig {
+    // Security 스키마 설정
+    SecurityScheme bearerAuth = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("Bearer")
+        .bearerFormat("JWT")
+        .in(SecurityScheme.In.HEADER)
+        .name(HttpHeaders.AUTHORIZATION);
+
+    // Security 요청 설정
+    SecurityRequirement addSecurityItem = new SecurityRequirement().addList("JWT");
 
     @Bean
     public OpenAPI openAPI() {
@@ -15,6 +30,8 @@ public class SwaggerConfig {
         Server httpsServer = new Server().url("https://k10e204.p.ssafy.io").description("Prod HTTPS Server");
         return new OpenAPI()
                 .servers(Arrays.asList(httpsServer, localServer))
+                .components(new Components().addSecuritySchemes("JWT",bearerAuth))
+                .addSecurityItem(addSecurityItem)
                 .info(apiInfo());
     }
     private Info apiInfo() {
