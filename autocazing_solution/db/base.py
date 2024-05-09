4c10455b96a9d87c0a4c720d6e6df5f77067a8ba
김영후 <eagle7449@naver.com>
2024-05-10 00:02:14 +0900

    Fix: base.py 수정

--- autocazing_solution/db/base.py
+++ autocazing_solution/db/base.py
@@ -1,2 +1,3 @@
-from .models import orders
-from .models import restock_orders
+# 다수의 데이터 모델을 한고셍서 연동할 수 있도록 하는 파일
+from .models.orders import Orders
+from .models.restock_orders import RestockOrders
