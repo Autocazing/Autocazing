a3b3fc8d413fba64ba510766531d29016df7249d
김영후 <eagle7449@naver.com>
2024-05-09 23:58:36 +0900

    Feat: RestockOrder, RestockOrderSpecifics class 생성

--- autocazing_solution/db/models/orders.py
+++ autocazing_solution/db/models/orders.py
@@ -1,18 +1,19 @@
 from sqlalchemy import *
 from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
 class OrderSpecific(Base):
     __tablename__ = 'order_specifics'
-    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
+    order_id = Column(Integer, ForeignKey('orders.order_id'), nullable=False)
     menu_id = Column(Integer, nullable=False)
     menu_quantity = Column(Integer, nullable=False)
     menu_price = Column(Integer, nullable=False)
 
 class Orders(Base):
     __tablename__ = 'orders'
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     store_id = Column(Integer, nullable=False)
     order_specifics = relationship("OrderSpecific", back_populates="order", cascade="all, delete-orphan")
 
---
+++ autocazing_solution/db/models/restock_orders.py
@@ -0,0 +1,31 @@
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql import func
+from enum import Enum as PyEnum
+
+
+Base = declarative_base()
+
+class RestockStatus(PyEnum):
+    WRITING = "WRITING"
+    ORDERED = "ORDERED"
+    ON_DELIVERY = "ON_DELIVEREY"
+    ARRIVED = "ARRIVED"
+    COMPLETE = "COMPLETE"
+
+class RestockOrderSpecific(Base):
+    __tablename__ = 'restock_order_specifics'
+    restock_order_id = Column(Integer, ForeignKey('restock_orders.restock_order_id'), nullable=False)
+    ingredient_id = Column(Integer, nullable=False)
+    ingredient_quantity = Column(Integer, nullable=False)
+    ingredient_price = Column(Integer, nullable=False)
+
+class RestockOrders(Base):
+    __tablename__ = 'restock_orders'
+    restock_order_id = Column(Integer, primary_key=True, autoincrement=True)
+    store_id = Column(Integer, nullable=false)
+    restock_order_specifics = relationship("RestockOrderSpecific", back_populates="restock_order", cascade="all, delete-orphan")
+    status = Column(Enum(RestockStatus), default=RestockStatus.WRITING, nullable=False)
+
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
