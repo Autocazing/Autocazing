67db9d821084ed5e764529b8e3fc64d5c30ee8e9
kimmin <kimmin5714@gmail.com>
2024-05-10 22:05:55 +0900

    Fix: 매출 오늘 날짜부터 조회

--- server/src/main/java/e204/autocazing/sale/service/SaleService.java
+++ server/src/main/java/e204/autocazing/sale/service/SaleService.java
@@ -21,20 +21,20 @@ public class SaleService {
 		List<Map<String, Object>> saleDtoList = new ArrayList<>();
 
 		//더미데이터 기준
-		String dateTimeString = "2024-05-08 17:00:00";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		LocalDateTime baseDate = LocalDateTime.parse(dateTimeString, formatter);
+		// String dateTimeString = "2024-05-08 17:00:00";
+		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		// LocalDateTime baseDate = LocalDateTime.parse(dateTimeString, formatter);
 
 		if(type.equals("day")){
-			LocalDateTime currentTime = baseDate.minusDays(30);
+			LocalDateTime currentTime = LocalDateTime.now().minusDays(30);
 			saleDtoList = orderRepository.calculateDailySales(currentTime);
 		}
 		else if(type.equals("week")){
-			LocalDateTime currentTime = baseDate.minusWeeks(12);
+			LocalDateTime currentTime = LocalDateTime.now().minusWeeks(12);
 			saleDtoList = orderRepository.calculateWeekSales(currentTime);
 		}
 		else if(type.equals("month")){
-			LocalDateTime currentTime = baseDate.minusMonths(12);
+			LocalDateTime currentTime = LocalDateTime.now().minusMonths(12);
 			saleDtoList = orderRepository.calculateMonthSales(currentTime);
 		}
 
@@ -43,10 +43,10 @@ public class SaleService {
 
 	public Integer getSoldNumber() {
 		//더미데이터 기준
-		String dateTimeString = "2024-05-08 17:00:00";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		LocalDateTime baseDate = LocalDateTime.parse(dateTimeString, formatter);
-		LocalDate currentDay = LocalDate.from(baseDate);
+		// String dateTimeString = "2024-05-08 17:00:00";
+		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		// LocalDateTime baseDate = LocalDateTime.parse(dateTimeString, formatter);
+		LocalDate currentDay = LocalDate.from(LocalDateTime.now());
 		return orderRepository.getSoldNumber(currentDay);
 
 	}
