ccb4dc3b935072cee8ae09f4ed1e15b1311f05ee
moonwlgh <moonwlgh@gmail.com>
2024-05-10 12:22:04 +0900

    Fix: Read Api 연결 및 구조 바꾸기 완료

--- client/src/pages/management/Company/CompanyTable.jsx
+++ client/src/pages/management/Company/CompanyTable.jsx
@@ -1,26 +1,9 @@
-import modifyIcon from "../../../images/orderlist/modify.svg";
-import deleteIcon from "../../../images/orderlist/delete.svg";
+import CompanyListInfo from "./CompanyListInfo";
 
-const CompanyList = [
-    // 테스트용
-    {
-        companyname: "지호상사",
-        managername: "문지호",
-        email: "moonwlfgh@naver.com",
-    },
-    {
-        companyname: "동민상사",
-        managername: "김영후",
-        email: "0hoo@naver.com",
-    },
-    {
-        companyname: "영후상사",
-        managername: "위동민",
-        email: "dsadadsad@naver.com",
-    },
-];
-
-const CompanyTable = () => {
+const CompanyTable = ({ companyInfo }) => {
+    if (!companyInfo || companyInfo.length === 0) {
+        return <div>No company data available.</div>;
+    }
     return (
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
             <div className="flex-col gap-3 flex sm:flex-row sm:items-center sm:justify-between">
@@ -56,50 +39,12 @@ const CompanyTable = () => {
                     </div>
                 </div>
 
-                {CompanyList.map((company, key) => (
-                    <div
-                        className={`grid grid-cols-4 sm:grid-cols-4 ${
-                            key === CompanyList.length - 1
-                                ? ""
-                                : "border-b border-stroke dark:border-strokedark"
-                        }`}
-                        key={key}
-                    >
-                        <div className="flex items-center justify-center p-2.5 xl:p-5">
-                            <p className="text-black dark:text-white">
-                                {company.companyname}
-                            </p>
-                        </div>
-
-                        <div className="flex items-center justify-center p-2.5 xl:p-5">
-                            <p className="text-black dark:text-white">
-                                {company.managername}
-                            </p>
-                        </div>
-
-                        <div className="items-center justify-center pt-2.5 pb-2.5 text-xs  sm:flex sm:text-base xl:p-5">
-                            <p className="text-center text-black dark:text-white">
-                                {company.email}
-                            </p>
-                        </div>
-
-                        <div className="items-center justify-center text-center p-2.5 sm:flex xl:p-5">
-                            <button className="mr-2 sm:ml-8">
-                                <img
-                                    className="w-5 h-5 sm:w-auto sm:h-auto"
-                                    src={modifyIcon}
-                                    alt="Modify"
-                                />
-                            </button>
-                            <button>
-                                <img
-                                    className="w-5 h-5 sm:w-auto sm:h-auto"
-                                    src={deleteIcon}
-                                    alt="delete"
-                                />
-                            </button>
-                        </div>
-                    </div>
+                {companyInfo.map((company, index) => (
+                    <CompanyListInfo
+                        key={company.venderId}
+                        company={company}
+                        isLastItem={index === companyInfo.length - 1}
+                    />
                 ))}
             </div>
         </div>
