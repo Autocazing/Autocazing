70169820d23c48828505c5c0d4d3683f15e8bf53
김영후 <eagle7449@naver.com>
2024-05-11 16:26:53 +0900

    Feat: eureka client 패키지 설치

--- autocazing_solution/main.py
+++ autocazing_solution/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from db.session import SessionLocal
+from py_eureka_client import eureka_client
 # from api import router as api_router
 
 app = FastAPI()
+
+# Eureka 서버 URL, FastAPI 애플리케이션의 호스트 & 포트 정보
+eureka_server = "http://your-eureka-server-host:port/eureka/"
+app_host = "app_host_address"
+app_port = 8000
+
+# Eureka에 등록
+eureka_client.init_registry_client(eureka_server=eureka_server,
+                                   app_name="fastapi-service",
+                                   instance_port=app_port,
+                                   instance_host=app_host)
 
 # Dependency
 def get_db():
