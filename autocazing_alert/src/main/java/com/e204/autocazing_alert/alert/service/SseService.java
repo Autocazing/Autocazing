01ea1ae360293f5a19c952b1b99a4f5893f7b75d
위동민 <dod212@naver.com>
2024-05-12 23:28:45 +0900

    Feat: SSE init

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/service/SseService.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/service/SseService.java
@@ -1,2 +1,33 @@
-package com.e204.autocazing_alert.alert.service;public class SseService {
+package com.e204.autocazing_alert.alert.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
+public class SseService {
+    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    //Emitter 객체 생성 후 연결이 오나료되거나 타임아웃될때 리스트에서 제거
+    //Long.MAX_VALUE 는 사실상 무한대
+    public SseEmitter createEmitter() {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        this.emitters.add(emitter);
+
+        emitter.onCompletion(() -> this.emitters.remove(emitter));
+        emitter.onTimeout(() -> this.emitters.remove(emitter));
+
+        return emitter;
+    }
+
+    public void sendNotificationToAll(String message) {
+        for (SseEmitter emitter : this.emitters) {
+            try {
+                emitter.send(SseEmitter.event().name("notification").data(message));
+            } catch (IOException e) {
+                emitter.completeWithError(e);
+            }
+        }
+    }
 }
