362b154c24ddff47d3cbe784891061f5a841e294
김민 <kimmin5714@gmail.com>
2024-05-12 19:09:05 +0900

    Revert "Merge branch 'inventory/sales' into 'be/inventory'"
    This reverts merge request !171

---
+++ client/src/pages/management/CompanyManagement.jsx
@@ -0,0 +1,37 @@
+import CompanyTable from "../../components/management/CompanyTable";
+import { useState } from "react";
+import CompanyManagementModal from "./CompanyManagementModal";
+
+const CompanyManagement = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    return (
+        <>
+            <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+                <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+                    업체 관리
+                </h2>
+                <ol className="flex items-center gap-2">
+                    <li>가게 관리 /</li>
+                    <li className="font-bold text-primary">업체 관리</li>
+                </ol>
+            </div>
+            <CompanyTable />
+            <button
+                onClick={() => setModalIsOpen(true)}
+                className="bg-transparent hover:bg-primary text-primary font-semibold hover:text-white py-2 px-4 border border-primary hover:border-transparent rounded"
+            >
+                업체추가
+            </button>
+
+            {modalIsOpen && (
+                <CompanyManagementModal
+                    isOpen={modalIsOpen}
+                    onClose={() => setModalIsOpen(false)}
+                />
+            )}
+        </>
+    );
+};
+
+export default CompanyManagement;
