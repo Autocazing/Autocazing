26d54299897f553b830acbd84c6a93b87253dcf0
김영후 <eagle7449@naver.com>
2024-05-12 15:52:21 +0900

    Feat: 월 예상 매출 schema 작성 및 api 생성

--- autocazing_solution/main.py
+++ autocazing_solution/main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 from db.session import SessionLocal
-from py_eureka_client import eureka_client
-# from api import router as api_router
+# from py_eureka_client import eureka_client
+from api.monthly_sales.monthly_sales_router import monthly_sales_router
 
 app = FastAPI(docs_url='/api/solution-service/docs', openapi_url='/api/solution-service/openapi.json')
 
-@app.on_event("startup")
-async def startup_event():
-    await eureka_client.init_async(
-        eureka_server="http://discovery-server:8761/eureka",
-        app_name="solution-service",
-        instance_port=8088,
-        instance_host="solution-service"
-)
+# 하위 api 라우터들 main에 추가
+app.include_router(monthly_sales_router, prefix="/api/solution")
+
+# @app.on_event("startup")
+# async def startup_event():
+#     await eureka_client.init_async(
+#         eureka_server="http://discovery-server:8761/eureka",
+#         app_name="solution-service",
+#         instance_port=8088,
+#         instance_host="solution-service"
+# )
 
 # Dependency
 def get_db():
@@ -26,8 +29,8 @@ def get_db():
 async def root():
     return {"message": "Hello World"}
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    eureka_client.stop()
+# @app.on_event("shutdown")
+# async def shutdown_event():
+#     eureka_client.stop()
 
 # app.include_router(api_router)
