e6cf3a61eb289d49ab2ed4045a2dcdefe51d62eb
김영후 <eagle7449@naver.com>
2024-05-12 23:16:45 +0900

    Fix: kafka-python 방식으로 수정

--- autocazing_solution/messaging/kafka_cosume_logic.py
+++ autocazing_solution/messaging/kafka_cosume_logic.py
@@ -7,11 +7,17 @@ async def consume_messages():
     current_loop = asyncio.get_running_loop()
     while True:
         message = await current_loop.run_in_executor(None, consumer.poll, 1.0)
-        if message is None:
+        if not message:
             continue
-        if message.error():
-            print(f"Kafka Consumer error: {message.error()}")
-            continue
-        # 예시 데이터 처리 로직
-        user_data = json.loads(message.value())
-        print("Received user data:", user_data)
+            
+        for tp, messages in message.items():
+            for message in messages:
+                if message.error:
+                    print(f"Kafka Consumer error: {message.error}")
+                    continue
+                
+                try:
+                    user_data = json.loads(message.value.decode('utf-8'))
+                    print("Received user data:", user_data)
+                except json.JSONDecodeError:
+                    print("Error decoding message JSON")
--- autocazing_solution/messaging/kafka_instance.py
+++ autocazing_solution/messaging/kafka_instance.py
@@ -1,11 +1,12 @@
 from kafka import KafkaProducer, KafkaConsumer
+from json import dumps
 
 # Kafka 프로듀서 인스턴스 생성
-producer = KafkaProducer(bootstrap_servers=['127.0.0.1:10000', '127.0.0.1:10001', '127.0.0.1:10002'])
+producer = KafkaProducer(bootstrap_servers=['127.0.0.1:10000', '127.0.0.1:10001', '127.0.0.1:10002'],
+                        value_serializer=lambda x:dumps(x).encode('utf-8'))  # 메시지의 값 직렬화 
 
 # Kafka 컨슈머 인스턴스 생성
-consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:10000', '127.0.0.1:10001', '127.0.0.1:10002'],
+consumer = KafkaConsumer('test',
+                        bootstrap_servers=['127.0.0.1:10000', '127.0.0.1:10001', '127.0.0.1:10002'],
                         auto_offset_reset="earliest",  # 오프셋 위치(메시지를 읽어오는 시점, earliest: 가장 처음(구독 전부터), latest: 가장 최근(구독 후부터))
                         group_id='myGroup')
-
-consumer.subscribe(['test'])
