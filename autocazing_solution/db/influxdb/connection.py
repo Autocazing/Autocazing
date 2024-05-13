1a0912d81daecf32accb027afb6bbc3c010907d3
김영후 <eagle7449@naver.com>
2024-05-14 00:28:17 +0900

    Feat: influx db 연결 instance 생성

---
+++ autocazing_solution/db/influxdb/connection.py
@@ -0,0 +1,19 @@
+from influxdb import InfluxDBClient
+
+
+class InfluxDBConnection:
+    def __init__(self, token: str):
+        self.token = token
+        self.client = None
+        self.connect()
+
+    def connect(self):
+        """Instantiate a connection to the InfluxDB."""
+        self.client = InfluxDBClient(username=None, password=None, headers={"Authorization": self.token})
+        print("Use authorization token: " + self.token)
+        version = self.client.ping()
+        print("Successfully connected to InfluxDB: " + version)
+
+    def get_client(self) -> InfluxDBClient:
+        """Return the InfluxDB client instance."""
+        return self.client
