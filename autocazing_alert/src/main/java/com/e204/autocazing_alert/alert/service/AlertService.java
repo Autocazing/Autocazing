77bbefa598abb3e672fa4927d75cef340f98d896
위동민 <dod212@naver.com>
2024-05-13 23:32:16 +0900

    Feat:Alert Init

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/service/AlertService.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/service/AlertService.java
@@ -1,2 +1,36 @@
-package com.e204.autocazing_alert.alert.service;public class AlertService {
+package com.e204.autocazing_alert.alert.service;
+
+import com.e204.autocazing_alert.alert.db.AlertEntity;
+import com.e204.autocazing_alert.alert.dto.AlertDetailsDto;
+import com.e204.autocazing_alert.alert.repository.AlertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AlertService {
+
+    @Autowired
+    private AlertRepository alertRepository;
+
+    public List<AlertDetailsDto> findAllAlert(String loginId) {
+        List<AlertEntity> alertEntities =alertRepository.findByLoginId(loginId);
+        return alertEntities
+                .stream()
+                .map(this::convertToAlertDetailsDTO)
+                .collect(Collectors.toList());
+    }
+
+    private AlertDetailsDto convertToAlertDetailsDTO(AlertEntity alert) {
+        AlertDetailsDto alertDetails = new AlertDetailsDto();
+        alertDetails.setAlertId(alert.getAlertId());
+        alertDetails.setContent(alert.getContent());
+        alertDetails.setCreatedAt(alert.getCreatedAt());
+        alertDetails.setUpdatedAt(alert.getUpdatedAt());
+        alertDetails.setCompleted(alert.getCompleted());
+        return alertDetails;
+    }
+
 }
