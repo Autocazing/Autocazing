407501fc9a1b5018f14ca6d88825a2218832d25f
김영후 <eagle7449@naver.com>
2024-05-14 21:49:26 +0900

    Feat: Report 테이블 작성

--- autocazing_solution/db/mysql/models/reports.py
+++ autocazing_solution/db/mysql/models/reports.py
@@ -3,11 +3,26 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from db.mysql.base import Base
 
-class MenuSalesSpecific(Base):
-    __tablename__ = 'menu_sales_specific'
+# class MenuSalesSpecific(Base):
+#     __tablename__ = 'menu_sales_specific'
+#     report_id = Column(Integer, ForeignKey('reports.report_id'), nullable=False, primary_key=True)
 
-class ExpirationSpecific(Base):
+class ExpirationSpecific(Base): # 유통기한 임박 재료
     __tablename__ = 'expiration_specific'
+    ingridient_name = Column(String, nullable=False)
 
-class Reports(Base):
+class OnDeliveryIngredients(Base):  # 배송 중인 재료
+    __tablename__ = 'on_delivery_ingredients'
+    ingridient_name = Column(String, nullable=False)
+
+class Reports(Base):    # 리포트
     __tablename__ = 'reports'
+    report_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    store_id = Column(Integer, nullable=False)
+    sales = Column(Integer, nullable=False)
+    expected_monthly_sales = Column(Integer, nullable=False)
+    current_monthly_sales = Column(Integer, nullable=False) # 일단 애매함. 매번 새로운 값을 반영해야 하는데...
+    
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    
