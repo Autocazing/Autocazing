96f5f01be6fcfb74f7e358323bb74fc62af84199
위동민 <dod212@naver.com>
2024-05-14 14:17:16 +0900

    Fix: 알림 조회 기능수정

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/controller/AlertController.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/alert/controller/AlertController.java
@@ -15,10 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -59,8 +56,7 @@ public class AlertController {
     })
     //알림 조회하기 feat Id
     @GetMapping("/{loginId}")
-    public ResponseEntity findAllAlertsByLoginId(HttpServletRequest httpServletRequest){
-        String loginId = httpServletRequest.getHeader("loginId");
+    public ResponseEntity findAllAlertsByLoginId(@PathVariable(name = "loginId") String loginId){
         List<AlertDetailsDto> alertDetails = alertService.findAllAlert(loginId);
         return ResponseEntity.ok(alertDetails);
 
