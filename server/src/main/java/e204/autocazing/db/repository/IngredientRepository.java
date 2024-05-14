32b58d44a7b44834244171a993b54a016077c006
위동민 <dod212@naver.com>
2024-05-14 17:38:34 +0900

    Fix: Stock , Ingredient 연관 N:1 로 변경

--- server/src/main/java/e204/autocazing/db/repository/IngredientRepository.java
+++ server/src/main/java/e204/autocazing/db/repository/IngredientRepository.java
@@ -1,10 +1,15 @@
 package e204.autocazing.db.repository;
 
 import e204.autocazing.db.entity.IngredientEntity;
+import e204.autocazing.db.entity.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<IngredientEntity, Integer> {
     IngredientEntity findByIngredientName(String ingredientName);
+
+    List<IngredientEntity> findByStore(StoreEntity store);
 }
