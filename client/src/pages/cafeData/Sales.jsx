025a5b9fe13fa51116d4830084b46a46b60ec50c
진성민 <sengmin14@naver.com>
2024-05-14 20:29:13 +0900

    refactor : Sales.jsx 불필요한 코드 제거

--- client/src/pages/cafeData/Sales.jsx
+++ client/src/pages/cafeData/Sales.jsx
@@ -2,11 +2,6 @@ import { useState } from "react";
 import SalesChart from "../../components/cafeData/SalesChart.jsx";
 
 const Sales = () => {
-    const [list, setList] = useState([1, 2, 3, 4, 5]);
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
     return (
         <div>
             <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
@@ -18,16 +13,6 @@ const Sales = () => {
                     <li className="font-bold text-primary">매출별</li>
                 </ol>
             </div>
-            {/* <button className="dropdown-btn" onClick={toggleDropdown}>
-                Dropdown Button
-            </button>
-            {isOpen && (
-                <div className="dropdown-content">
-                    {list.map((item, index) => {
-                        return <li key={index}>{list[index]}</li>;
-                    })}
-                </div>
-            )} */}
             <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
                 <SalesChart />
             </div>
