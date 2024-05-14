50cc437faa7ce0c9e420b42df22237a16b6562e1
김영후 <eagle7449@naver.com>
2024-05-14 16:09:37 +0900

    Fix: influxdb 시작 시 로드 삭제

--- autocazing_solution/main.py
+++ autocazing_solution/main.py
@@ -6,22 +6,12 @@ import asyncio
 from messaging.kafka_instance import producer, consumer
 from api.monthly_sales.monthly_sales_router import monthly_sales_router
 from messaging.kafka_cosume_logic import consume_messages
-from db.influxdb.connection import InfluxDBConnection
 
 app = FastAPI(docs_url='/api/solution-service/docs', openapi_url='/api/solution-service/openapi.json')
 settings = get_setting()
 
 # 하위 api 라우터들 main에 추가
 app.include_router(monthly_sales_router, prefix="/api/solution")
-
-# InfluxDB 연결 객체 생성
-influx_connection = InfluxDBConnection(
-    host=settings.INFLUXDB_HOST,
-    port=settings.INFLUXDB_PORT,
-    username=settings.INFLUXDB_USER_NAME,
-    password=settings.INFLUXDB_USER_PASSWORD,
-    token=settings.INFLUXDB_USER_TOKEN
-)
 
 async def register_with_eureka():
     try:
@@ -35,13 +25,6 @@ async def register_with_eureka():
     except Exception as e:
         print(f"Failed to register with Eureka: {e}")
 
-async def connect_to_influxdb():
-    try:
-        influx_connection.connect()
-        print("Connected to InfluxDB")
-    except Exception as e:
-        print(f"Failed to connect to InfluxDB: {e}")
-
 async def start_kafka_consumer():
     try:
         asyncio.create_task(consume_messages())
@@ -53,7 +36,6 @@ async def start_kafka_consumer():
 async def startup_event():
     await asyncio.gather(
         register_with_eureka(),
-        connect_to_influxdb(),
         start_kafka_consumer()
     )
 
@@ -73,5 +55,4 @@ async def root():
 @app.on_event("shutdown")
 async def shutdown_event():
     consumer.close()  # Kafka 컨슈머 종료
-    influx_connection.client.close()
     eureka_client.stop()
