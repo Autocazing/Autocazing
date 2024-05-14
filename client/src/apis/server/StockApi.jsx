f1a869eb9b9c218f3ee52265acbf305294201249
moonwlgh <moonwlgh@gmail.com>
2024-05-14 15:36:33 +0900

    Feat: Stock 수정 삭제 api 정의

--- client/src/apis/server/StockApi.jsx
+++ client/src/apis/server/StockApi.jsx
@@ -32,4 +32,43 @@ const StockPostApi = () => {
     return mutation;
 };
 
-export { StockGetApi, StockPostApi };
+const StockEditApi = (stockId) => {
+    const queryClient = useQueryClient();
+
+    const fetchEdit = (editData) => {
+        return axiosInstance.put(`/stocks/${stockId}`, editData);
+    };
+
+    const mutation = useMutation({
+        mutationFn: fetchEdit,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["Getstock"] });
+            console.log("성공");
+        },
+        onError: (error) => {
+            console.error("실패", error);
+        },
+    });
+
+    return mutation;
+};
+
+const StockDeleteApi = (stockId) => {
+    const queryClient = useQueryClient();
+    const fetchDelete = () => {
+        return axiosInstance.delete(`/stocks/${stockId}`);
+    };
+    const mutation = useMutation({
+        mutationFn: fetchDelete,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["GetStock"] });
+            console.log("성공");
+        },
+        onError: (error) => {
+            console.error("실패", error);
+        },
+    });
+    return mutation;
+};
+
+export { StockGetApi, StockPostApi, StockDeleteApi, StockEditApi };
