8ef837f3786ec2572454dfd942b7da8866ce0772
김영후 <eagle7449@naver.com>
2024-05-15 13:28:58 +0900

    Feat: report router에 db session 의존 파라미터 추가

--- autocazing_solution/api/report/report_router.py
+++ autocazing_solution/api/report/report_router.py
@@ -1,15 +1,19 @@
+from fastapi import Depends
 from typing import List
 from fastapi import APIRouter
 from api.report.schema.report_response import ReportResponseSchema
+from sqlalchemy.orm import Session
+from main import get_db
 
 report_router = APIRouter(prefix="/report")
 
 @report_router.get("", tags=["report_router"], response_model=List[ReportResponseSchema])
-async def get_reports():
+async def get_reports(db: Session = Depends(get_db)):
     
     return []
 
 @report_router.get("/{report_id}", tags=["report_router"], response_model=ReportResponseSchema)
-async def get_report_details(report_id: int):
+async def get_report_details(report_id: int, db: Session = Depends(get_db)):
+
     
     return ReportResponseSchema
