bd619ec7438c7f4e43c93bccb9ddb6ef2d233240
김영후 <eagle7449@naver.com>
2024-05-15 14:55:17 +0900

    Fix: header에서 추출할 값 loginId로 수정

--- autocazing_solution/api/report/report_router.py
+++ autocazing_solution/api/report/report_router.py
@@ -13,10 +13,10 @@ report_router = APIRouter(prefix="/report")
 @report_router.get("", tags=["report_router"], response_model=List[ReportResponseSchema])
 async def get_reports(request: Request, month: Union[int, None] = Query(None, ge=1, le=12), db: Session = Depends(get_db)):
     db = next(db)
-    store_id = request.headers.get("storeId")
-    if store_id is None:
+    login_id = request.headers.get("loginId")
+    if login_id is None:
         raise HTTPException(status_code=400, detail="store_id header is required")
-    query = db.query(reports.Reports).filter(reports.Reports.store_id == store_id)  # 가게가 가진 모든 리포트 조회
+    query = db.query(reports.Reports).filter(reports.Reports.store_id == login_id)  # 가게가 가진 모든 리포트 조회
     if month is not None:
         query = query.filter(extract('month', reports.Reports.created_at) == month)
     
@@ -28,10 +28,10 @@ async def get_reports(request: Request, month: Union[int, None] = Query(None, ge
 @report_router.get("/{report_id}", tags=["report_router"], response_model=ReportResponseSchema)
 async def get_report_details(request: Request, report_id: int, db: Session = Depends(get_db)):
     db = next(db)
-    store_id = request.headers.get("storeId")
-    if store_id is None:
+    login_id = request.headers.get("loginId")
+    if login_id is None:
         raise HTTPException(status_code=400, detail="store_id header is required")
-    store_report_detail = db.query(reports.Reports).filter(reports.Reports.store_id == store_id).filter(reports.Reports.report_id == report_id).first()
+    store_report_detail = db.query(reports.Reports).filter(reports.Reports.store_id == login_id).filter(reports.Reports.report_id == report_id).first()
     if store_report_detail is None:
         raise HTTPException(status_code=404, detail="Report not found")
     
