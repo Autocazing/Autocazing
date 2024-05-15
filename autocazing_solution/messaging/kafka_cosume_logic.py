db6858b362ba9198d3c2ee46ffe94ade4b725853
김영후 <eagle7449@naver.com>
2024-05-15 20:59:25 +0900

    Fix: Order kafka 로직 작성 및 OrderSpecifics 테이블 수정

--- autocazing_solution/messaging/kafka_cosume_logic.py
+++ autocazing_solution/messaging/kafka_cosume_logic.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 from json import loads
 from db.mysql.session import mysqlSession
 from db.mysql.models.ingredients import Ingredients
+from db.mysql.models.menus import Menus
+from db.mysql.models.orders import Orders, OrderSpecifics
 
 
 # 각 토픽별 Kafka 컨슈머 생성
@@ -31,9 +33,9 @@ async def process_ingredient_message(key: str, value: str):
     try:
         new_ingredient = Ingredients(
             login_id=key,
-            ingredient_name = value["ingredient_name"],
-            ingredient_price = value["ingredient_price"],
-            order_count = value["order_count"]
+            ingredient_name = value["ingredientName"],
+            ingredient_price = value["ingredientPrice"],
+            order_count = value["orderCount"]
         )
         db.add(new_ingredient)
         db.commit()
@@ -46,18 +48,55 @@ async def consume_menu_messages():
     try:
         async for message in menu_consumer:
             print("Received from menu:", message.value)
-            # menu 메시지 처리 로직 추가
+            await process_menu_message(message.key, message.value)
     finally:
         await menu_consumer.stop()
+
+async def process_menu_message(key: str, value: dict):
+    db: Session = get_db_session()
+    try:
+        new_menu = Menus(
+            login_id=key,
+            menu_name=value["menuName"],
+            menu_price=value["menuPrice"],
+            on_event=value["onEvent"],
+            discount_rate=value["discountRate"]
+        )
+        db.add(new_menu)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error processing menu message: {e}")
+    finally:
+        db.close()
 
 async def consume_order_messages():
     await order_consumer.start()
     try:
         async for message in order_consumer:
             print("Received from order:", message.value)
-            # order 메시지 처리 로직 추가
+            await process_order_message(message.key, message.value)
     finally:
         await order_consumer.stop()
+
+async def process_order_message(key: str, value: dict):
+    db: Session = get_db_session()
+    try:
+        new_order = Orders(
+            login_id=key,
+            order_specifics=[OrderSpecifics(
+                menu_id=spec["menuName"],
+                menu_quantity=spec["menuQuantity"],
+                menu_price=spec["menuPrice"]
+            ) for spec in value["orderSpecifics"]]
+        )
+        db.add(new_order)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error processing order message: {e}")
+    finally:
+        db.close()
 
 async def consume_restock_order_messages():
     await restock_order_consumer.start()
