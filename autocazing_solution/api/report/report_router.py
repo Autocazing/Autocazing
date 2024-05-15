8d3bd763beffd5d5bb26a60f66bd9e28449498d2
김영후 <eagle7449@naver.com>
2024-05-15 15:05:24 +0900

    Fix: loginId 조회로 수정

--- autocazing_solution/api/report/report_router.py
+++ autocazing_solution/api/report/report_router.py
@@ -15,8 +15,8 @@ async def get_reports(request: Request, month: Union[int, None] = Query(None, ge
     db = next(db)
     login_id = request.headers.get("loginId")
     if login_id is None:
-        raise HTTPException(status_code=400, detail="store_id header is required")
-    query = db.query(reports.Reports).filter(reports.Reports.store_id == login_id)  # 가게가 가진 모든 리포트 조회
+        raise HTTPException(status_code=400, detail="loginId header is required")
+    query = db.query(reports.Reports).filter(reports.Reports.login_id == login_id)  # 가게가 가진 모든 리포트 조회
     if month is not None:
         query = query.filter(extract('month', reports.Reports.created_at) == month)
     
@@ -30,8 +30,8 @@ async def get_report_details(request: Request, report_id: int, db: Session = Dep
     db = next(db)
     login_id = request.headers.get("loginId")
     if login_id is None:
-        raise HTTPException(status_code=400, detail="store_id header is required")
-    store_report_detail = db.query(reports.Reports).filter(reports.Reports.store_id == login_id).filter(reports.Reports.report_id == report_id).first()
+        raise HTTPException(status_code=400, detail="loginId header is required")
+    store_report_detail = db.query(reports.Reports).filter(reports.Reports.login_id == login_id).filter(reports.Reports.report_id == report_id).first()
     if store_report_detail is None:
         raise HTTPException(status_code=404, detail="Report not found")
     
