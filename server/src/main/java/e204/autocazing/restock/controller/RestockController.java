80e79fe7a52e16432561262b1534080a7ec96930
kimmin <kimmin5714@gmail.com>
2024-05-15 21:31:32 +0900

    Fix: sms controller

--- server/src/main/java/e204/autocazing/restock/controller/RestockController.java
+++ server/src/main/java/e204/autocazing/restock/controller/RestockController.java
@@ -146,27 +146,5 @@ public class RestockController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/sms/{restockOrderId}/{venderId}/start")
-    public ResponseEntity<List<UpdatedRestockSpecificDto>> restockOrderStart(@PathVariable(name = "restockOrderId") Integer restockOrderId,
-        @PathVariable(name = "venderId") Integer venderId,
-        HttpServletRequest httpServletRequest) {
 
-        //restockOrderId의 venderId가 같은 orderSpecific의 status를 ON_DELIVERY로 변경
-        List<UpdatedRestockSpecificDto> updatedRestockSpecificDtoList
-            = restockSpecificService.updateRestockOrderSpecificStatus(restockOrderId, venderId, RestockOrderSpecificEntity.RestockSpecificStatus.ON_DELIVERY);
-
-        return ResponseEntity.ok(updatedRestockSpecificDtoList);
-    }
-
-    @GetMapping("/sms/{restockOrderId}/{venderId}/arrive")
-    public ResponseEntity<List<UpdatedRestockSpecificDto>> restockOrderArrive(@PathVariable(name = "restockOrderId") Integer restockOrderId,
-        @PathVariable(name = "venderId") Integer venderId,
-        HttpServletRequest httpServletRequest) {
-
-        //restockOrderId의 venderId가 같은 orderSpecific의 status를 ARRIVED로 변경
-        List<UpdatedRestockSpecificDto> updatedRestockSpecificDtoList
-            = restockSpecificService.updateRestockOrderSpecificStatus(restockOrderId, venderId, RestockOrderSpecificEntity.RestockSpecificStatus.ARRIVED);
-
-        return ResponseEntity.ok(updatedRestockSpecificDtoList);
-    }
 }
