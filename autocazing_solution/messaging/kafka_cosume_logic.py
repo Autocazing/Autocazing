3f73d709dabe8d7588e0bf903b7fa793574446e0
김영후 <eagle7449@naver.com>
2024-05-15 20:36:07 +0900

    Feat: 카프카 메시지 수신 시 재료 저장 로직 구현

--- autocazing_solution/messaging/kafka_cosume_logic.py
+++ autocazing_solution/messaging/kafka_cosume_logic.py
@@ -1,5 +1,10 @@
+from fastapi import Depends
 import asyncio
 from messaging.kafka_instance import create_consumer
+from sqlalchemy.orm import Session
+from json import loads
+from db.mysql.session import mysqlSession
+from db.mysql.models.ingredients import Ingredients
 
 
 # 각 토픽별 Kafka 컨슈머 생성
@@ -8,6 +13,8 @@ menu_consumer = create_consumer('menu')
 order_consumer = create_consumer('order')
 restock_order_consumer = create_consumer('restock_order')
 
+def get_db_session() -> Session:
+    return next(mysqlSession.get_db())
 
 # 비동기 kafka 메시지 소비
 async def consume_ingredient_messages():
@@ -15,9 +22,31 @@ async def consume_ingredient_messages():
     try:
         async for message in ingredient_consumer:
             print("Received from ingredient:", message.value)
-            # ingredient 메시지 처리 로직 추가
+            await process_ingredient_message(message.key, message.value)
     finally:
         await ingredient_consumer.stop()
+
+async def process_ingredient_message(key: str, value: str):
+    db: Session = get_db_session()
+    try:
+        # 만약 value가 이미 dict라면, loads를 사용하지 않고 바로 사용
+        if isinstance(value, str):
+            print("dict가 아님")
+            message_data = loads(value)
+        else:
+            print("dict임")
+            message_data = value
+        new_ingredient = Ingredients(
+            login_id=key,
+            ingredient_name=message_data["ingredient_name"],
+            ingredient_price=message_data["ingredient_price"],
+            order_count=message_data["order_count"]
+        )
+        db.add(new_ingredient)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error processing ingredient message: {e}")
 
 async def consume_menu_messages():
     await menu_consumer.start()
