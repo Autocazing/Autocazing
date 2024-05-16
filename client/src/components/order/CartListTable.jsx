392f5d71e77fe70ec946d035c2a61f8b1d2d9acc
진성민 <sengmin14@naver.com>
2024-05-16 15:54:25 +0900

    fix : 장바구니 리스트 사진 제거

--- client/src/components/order/CartListTable.jsx
+++ client/src/components/order/CartListTable.jsx
@@ -96,10 +96,6 @@ const CartListTable = () => {
                         key={key}
                     >
                         <div className="flex items-center gap-3 p-2.5 xl:p-5">
-                            <div className="flex-shrink-0">
-                                사진
-                                {/* <img src={brand.logo} alt="Brand" /> */}
-                            </div>
                             <p className="hidden text-black dark:text-white sm:block">
                                 {order.name}
                             </p>
