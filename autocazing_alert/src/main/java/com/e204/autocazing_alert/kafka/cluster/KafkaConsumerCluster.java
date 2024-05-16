b5d9f9a0f4eb97d41663417d223c6a82dfd6ee0b
kimmin <kimmin5714@gmail.com>
2024-05-16 16:02:35 +0900

    Fix: 카프카 연결

--- autocazing_alert/src/main/java/com/e204/autocazing_alert/kafka/cluster/KafkaConsumerCluster.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/kafka/cluster/KafkaConsumerCluster.java
@@ -2,12 +2,12 @@ package com.e204.autocazing_alert.kafka.cluster;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-import org.springframework.kafka.support.KafkaHeaders;
 
 import com.e204.autocazing_alert.kafka.entity.DeliveryRefreshEntity;
 import com.e204.autocazing_alert.kafka.entity.IngredientWarnEntity;
@@ -17,24 +17,24 @@ import com.e204.autocazing_alert.kafka.entity.SalesRefreshEntity;
 @Component
 public class KafkaConsumerCluster {
 
-	@KafkaListener(topics = "test", groupId = "${spring.kafka.consumer.group-id}")
-	public void ingredient_warn(@Payload IngredientWarnEntity message,
+	@KafkaListener(topics = "ingredient_warn", groupId = "${spring.kafka.consumer.group-id}")
+	public void ingredientWarn(@Payload IngredientWarnEntity message,
 		@Headers MessageHeaders messageHeaders,
 		@Header(KafkaHeaders.RECEIVED_KEY) String key) {
 
 		log.info("consumer: success >>> key: {}, message: {}, headers: {}", key, message.toString(), messageHeaders);
 	}
 
-	@KafkaListener(topics = "test", groupId = "${spring.kafka.consumer.group-id}")
-	public void sales_refresh(@Payload SalesRefreshEntity message,
+	@KafkaListener(topics = "sales_refresh", groupId = "${spring.kafka.consumer.group-id}")
+	public void salesRefresh(@Payload SalesRefreshEntity message,
 		@Headers MessageHeaders messageHeaders,
 		@Header(KafkaHeaders.RECEIVED_KEY) String key) {
 
 		log.info("consumer: success >>> key: {}, message: {}, headers: {}", key, message.toString(), messageHeaders);
 	}
 
-	@KafkaListener(topics = "test", groupId = "${spring.kafka.consumer.group-id}")
-	public void sales_refresh(@Payload DeliveryRefreshEntity message,
+	@KafkaListener(topics = "delivery_refresh", groupId = "${spring.kafka.consumer.group-id}")
+	public void deliveryRefresh(@Payload DeliveryRefreshEntity message,
 		@Headers MessageHeaders messageHeaders,
 		@Header(KafkaHeaders.RECEIVED_KEY) String key) {
 
