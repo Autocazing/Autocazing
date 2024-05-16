6d159c051592ddb1f9928ed8fc28ecd3eca59f15
김영후 <eagle7449@naver.com>
2024-05-16 20:48:19 +0900

    Feat: kafka consumer abstract class 생성

---
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/kafka/entity/ConsumerEntity.java
@@ -0,0 +1,9 @@
+package com.e204.autocazing_alert.kafka.entity;
+
+import lombok.Data;
+
+@Data
+public abstract class ConsumerEntity {
+
+
+}
--- autocazing_alert/src/main/java/com/e204/autocazing_alert/kafka/entity/DeliveryRefreshEntity.java
+++ autocazing_alert/src/main/java/com/e204/autocazing_alert/kafka/entity/DeliveryRefreshEntity.java
@@ -7,4 +7,5 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class DeliveryRefreshEntity {
+    private String message;
 }
