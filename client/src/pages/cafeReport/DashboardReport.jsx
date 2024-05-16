0e635db7bbacb37cf6d4c29d690acc98fb9388cb
문지호 <moonwlgh@gmail.com>
2024-05-17 00:28:54 +0900

    Feat: 솔루션을 위한 툴팁 설정

--- client/src/pages/cafeReport/DashboardReport.jsx
+++ client/src/pages/cafeReport/DashboardReport.jsx
@@ -5,7 +5,8 @@ const DashboardReport = () => {
     const today = new Date();
     const year = today.getFullYear();
     const month = today.getMonth() + 1;
-    const day = today.getDate();
+    // const day = today.getDate();
+    const day = 16;
     const {
         data: monthReportData,
         isLoading,
---
+++ client/src/pages/cafeReport/SolutionTooltip.jsx
@@ -0,0 +1,9 @@
+const SolutionTooltip = ({ item }) => {
+    return (
+        <div>
+            <p>{item.ingredient_name}에 대한 솔루션 내용</p>
+        </div>
+    );
+};
+
+export default SolutionTooltip;
