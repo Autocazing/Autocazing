003738ac87ac7d7e1d353c413e339c9c3763437a
진성민 <sengmin14@naver.com>
2024-05-16 20:24:36 +0900

    fix : 모달 창 내부 재료명 value -> defaultValue로 변경

--- client/src/pages/order/MaterialManagementModal.jsx
+++ client/src/pages/order/MaterialManagementModal.jsx
@@ -75,7 +75,7 @@ const MaterialManagementModal = ({ isOpen, onClose, initialValue }) => {
                         </label>
                         <input
                             name="ingredientName"
-                            value="여기에 재료명 가져와야 함"
+                            defaultValue="여기에 재료명 가져와야 함"
                             className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
