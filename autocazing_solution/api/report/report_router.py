0be777ceacd6145297182f7f9c2e5fca855b3cd5
김영후 <eagle7449@naver.com>
2024-05-16 11:40:57 +0900

    Fix: 일자별 조회 쿼리 추가

--- autocazing_solution/api/report/report_router.py
+++ autocazing_solution/api/report/report_router.py
@@ -11,14 +11,17 @@ from core.dependencies import get_db
 report_router = APIRouter(prefix="/report")
 
 @report_router.get("", tags=["report_router"], response_model=List[ReportResponseSchema])
-async def get_reports(request: Request, year: Union[int, None] = Query(None), month: Union[int, None] = Query(None, ge=1, le=12), db: Session = Depends(get_db)):
+async def get_reports(request: Request, year: Union[int, None] = Query(None), month: Union[int, None] = Query(None, ge=1, le=12), day: Union[int, None] = Query(None, ge=1, le=31), db: Session = Depends(get_db)):
     login_id = request.headers.get("loginId")
     if login_id is None:
         raise HTTPException(status_code=400, detail="loginId header is required")
     query = db.query(reports.Reports).filter(reports.Reports.login_id == login_id)  # 가게가 가진 모든 리포트 조회
-    if year is not None and month is not None:
+    if year is not None:    # 년도 필터링
         query = query.filter(extract('year', reports.Reports.created_at) == year)
+    if month is not None:   # 월 필터링
         query = query.filter(extract('month', reports.Reports.created_at) == month)
+    if day is not None: # 일 필터링
+        query = query.filter(extract('day', reports.Reports.created_at) == day)
     
     store_reports = query.all()
     store_reports.sort(key=lambda report: report.created_at.day)
