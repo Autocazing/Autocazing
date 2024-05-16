6c8bf459cd7567e0ba029974d2c7d0adab8344a0
kimmin <kimmin5714@gmail.com>
2024-05-16 17:56:51 +0900

    Feat: kafka

--- server/src/main/java/e204/autocazing/kafka/entity/SalesRefreshEntity.java
+++ server/src/main/java/e204/autocazing/kafka/entity/SalesRefreshEntity.java
@@ -1,9 +1,12 @@
 package e204.autocazing.kafka.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SalesRefreshEntity {
+	String message;
 }
