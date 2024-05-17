2ad14219948c9e8e3cb655220e2913224dcf2115
김영후 <eagle7449@naver.com>
2024-05-17 09:37:37 +0900

    Feat: 예상 월매출 테이블 생성

---
+++ autocazing_solution/db/mysql/models/predicted_sales.py
@@ -0,0 +1,10 @@
+from sqlalchemy import *
+from sqlalchemy.sql import func
+from db.mysql.base import Base
+
+class PredictedSales(Base):
+    __tablename__ = 'menus'
+    year_month = Column(String(length=6), primary_key=True, index=True, autoincrement=True)
+
+    login_id = Column(Integer, nullable=False)
+    predicted_monthly_sales = Column(Integer, nullable=False)
