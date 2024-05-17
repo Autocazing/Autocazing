e042d97f74b4f19e4b5cc9ed7f9179a3fe2d8055
김영후 <eagle7449@naver.com>
2024-05-18 00:30:39 +0900

    Fix: solution api 수정

--- autocazing_solution/api/ingredient_solution/ingredient_solution_router.py
+++ autocazing_solution/api/ingredient_solution/ingredient_solution_router.py
@@ -1,40 +1,33 @@
 from fastapi import Request, Query, Depends, HTTPException
-from typing import Union
-from typing import List
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from sqlalchemy import extract, func
 from core.dependencies import get_db
 from api.ingredient_solution.schema.ingredient_solution_response import IngredientSolutionResponse
-from db.mysql.models.menu_ingredients import MenuIngredients
-from db.mysql.models.menus import Menus
-from db.mysql.models.reports import Reports, ExpirationSpecifics
-from pulp import LpMaximize, LpProblem, LpVariable, lpSum, LpStatus, value
+from db.mysql.models.reports import IngredientSolution
 
 
-ingredient_solution_router = APIRouter(prefix="/ingredient-solution")
+ingredient_solution_router = APIRouter(prefix="{report_id}/ingredient-solution")
 
 @ingredient_solution_router.get("/{ingredient_id}", tags=["ingreient_solution_router"], response_model=IngredientSolutionResponse)
-async def get_ingredient_solution(request: Request, ingredient_id: int, db: Session = Depends(get_db)):
+async def get_ingredient_solution(request: Request, report_id: int, ingredient_id: int, db: Session = Depends(get_db)):
     login_id = request.headers.get("loginId")
     if login_id is None:
         raise HTTPException(status_code=400, detail="loginId header is required")
-    # 주어진 ingredient_id로 메뉴를 검색
-    menus = db.query(MenuIngredients.menu_id).filter(MenuIngredients.ingredient_id == ingredient_id).all()
-    if not menus:
-        raise HTTPException(status_code=404, detail="No menus found for the given ingredient_id")
-    menu_ids = [menu.menu_id for menu in menus]
-
-    optimal_sales_temp = {
-        "test1" : 30.0,
-        "test2" : 40.0,
-        "test3" : 60.0
-    }
     
+    # 주어진 report_id와 ingredient_id로 최적화 결과 조회
+    ingredient_solutions = db.query(IngredientSolution).filter(
+        IngredientSolution.report_id == report_id,
+        IngredientSolution.ingredient_id == ingredient_id
+    ).all()
+
+    if not ingredient_solutions:
+        raise HTTPException(status_code=404, detail="No ingredient solution found for the given report_id and ingredient_id")
+
+    optimal_sales = {solution.menu_name: solution.sale_quantity for solution in ingredient_solutions}
+
     ingredient_solution = IngredientSolutionResponse(
         status="Optimal",
-        optimal_sales=optimal_sales_temp
+        optimal_sales=optimal_sales
     )
 
     return ingredient_solution
-
