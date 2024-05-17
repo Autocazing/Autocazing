3d4970ab5661eb4e7d30d18995398dc90780d2e9
moonwlgh <moonwlgh@gmail.com>
2024-05-17 17:43:13 +0900

    Feat: 캘린더 axios 연결

--- client/src/pages/cafeReport/Calendar.jsx
+++ client/src/pages/cafeReport/Calendar.jsx
@@ -2,16 +2,35 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import "./Calendar.css";
 import ReportModal from "./ReportModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { ReportMonthGetApi } from "../../apis/server/ReportApi";
 const Calendar = ({ reportData }) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [selectedData, setSelectedData] = useState(null);
+    const [year, setYear] = useState("");
+    const [month, setMonth] = useState("");
+
+    const {
+        data: monthReportData,
+        isLoading,
+        isError,
+        error,
+    } = ReportMonthGetApi(year, month);
+
+    useEffect(() => {
+        console.log(monthReportData);
+    }, [monthReportData]);
 
     const events = reportData.map((data) => ({
         title: `${data.date} 리포트`,
         start: data.date,
         allDay: true,
     }));
+    const handleDatesSet = (dateInfo) => {
+        setMonth(dateInfo.startStr.slice(5, 7)); // 월 추출
+        setYear(dateInfo.startStr.slice(0, 4)); // 연도 추출
+    };
 
     // 클릭한 날짜와 데이터 안의 날짜가 일치하는지 찾고 모달 여는 함수
     const handleEventClick = (clickInfo) => {
@@ -31,6 +50,7 @@ const Calendar = ({ reportData }) => {
                     contentHeight={600}
                     events={events}
                     eventClick={handleEventClick}
+                    datesSet={handleDatesSet}
                 />
                 {selectedData && (
                     <ReportModal
