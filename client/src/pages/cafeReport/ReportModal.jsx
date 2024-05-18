35774c074a5c06c5d0321d6f3647b1eaa9ed0b1a
문지호 <moonwlgh@gmail.com>
2024-05-18 17:52:05 +0900

    Feat: 캘린더 리포트탭 완료

--- client/src/pages/cafeReport/ReportModal.jsx
+++ client/src/pages/cafeReport/ReportModal.jsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import ExpirationList from "./ExpirationList";
 
 const customStyles = {
     overlay: {
@@ -12,7 +13,7 @@ const customStyles = {
     },
     content: {
         width: "30rem",
-        height: "70%",
+
         zIndex: "150",
         position: "absolute",
         top: "50%",
@@ -24,12 +25,30 @@ const customStyles = {
         padding: "20px",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "space-between",
+        justifyContent: "start",
         overflow: "auto",
     },
 };
 
 const ReportModal = ({ isOpen, onClose, data }) => {
+    // 날짜 포맷팅
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date
+            .toLocaleDateString("ko-KR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            })
+            .replace(/\./g, "")
+            .replace(/(\d{4})(\d{2})(\d{2})/, "$1.$2.$3");
+    };
+
+    const formattedExpectedSales =
+        data.expected_monthly_sales.toLocaleString("ko-KR");
+    const formattedCurrentSales =
+        data.current_monthly_sales.toLocaleString("ko-KR");
+
     return (
         <Modal
             isOpen={isOpen}
@@ -37,25 +56,101 @@ const ReportModal = ({ isOpen, onClose, data }) => {
             style={customStyles}
             contentLabel="Report Details"
             ariaHideApp={false}
+            shouldCloseOnOverlayClick={true}
         >
-            <div>
+            <div className=" col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
                 <div>
-                    <h1 className="text-3xl my-4 font-semibold text-black dark:text-white">
-                        {data.date} 리포트
+                    <h1 className="text-3xl mb-8 font-semibold text-black dark:text-white">
+                        {formatDate(data.created_at)} 리포트
                     </h1>
                 </div>
                 <div>
-                    <p>어제: {data.yesterday}</p>
-                    <p>예측: {data.predict}</p>
-                </div>
-            </div>
+                    <div className="mb-4">
+                        <div className="font-semibold text-black text-title-sm mb-0">
+                            예상 매출액
+                        </div>
+                        <div className="text-title-xsm">
+                            AI 할용해 매출액을 예상 합니다.
+                        </div>
+                        <div className="flex flex-row justify-center gap-10 mt-3">
+                            <div className="text-center">
+                                <div className=" text-title-sm font-semibold text-primary">
+                                    {formattedExpectedSales}
+                                </div>
+                                <div className="text-title-xsm">
+                                    예상 월 매출액(원)
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <div className="text-black text-title-sm font-semibold text-primary">
+                                    {formattedCurrentSales}
+                                </div>
+                                <div className="text-title-xsm">
+                                    누적 월 매출액(원)
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-            <button
-                onClick={onClose}
-                className="mt-auto mx-auto inline-flex items-center justify-center rounded-md bg-primary py-2 px-4 text-center font-medium text-white hover:bg-opacity-90 focus:outline-none"
-            >
-                닫기
-            </button>
+                    <div className="mb-4">
+                        <div className="font-semibold text-black text-title-sm mb-0">
+                            배송중인 재료
+                        </div>
+                        <div className="text-title-xsm mb-3">
+                            배송중인 재료를 나타냅니다.
+                        </div>
+
+                        {data.on_delivery_ingredients.length > 0 ? (
+                            <div className="flex flex-justify pl-5 text-center justify-center gap-4">
+                                {data.on_delivery_ingredients.map(
+                                    (ingredient, index) => (
+                                        <div
+                                            key={index}
+                                            className="text-primary text-title-sm font-semibold"
+                                        >
+                                            {ingredient.ingredient_name}
+                                        </div>
+                                    ),
+                                )}
+                            </div>
+                        ) : (
+                            <div className="flex flex-justify pl-5 text-center justify-center gap-4">
+                                배송중인 재료가 없습니다.
+                            </div>
+                        )}
+                    </div>
+                    <div className="mb-4">
+                        <div className="font-semibold text-black text-title-sm mb-0">
+                            유통기한 임박 재료
+                        </div>
+                        <div className="text-title-xsm mb-3">
+                            유통기한 임박 재료에 대한 솔루션을 제공합니다.
+                        </div>
+
+                        {data.expiration_specifics.length > 0 ? (
+                            <div>
+                                {data.expiration_specifics.map((item) => (
+                                    <ExpirationList
+                                        key={item.ingredient_id}
+                                        item={item}
+                                        reportId={data.report_id}
+                                    />
+                                ))}
+                            </div>
+                        ) : (
+                            <div className="flex flex-justify pl-5 text-center justify-center gap-4">
+                                유통기한 임박 재료가 없습니다.
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <button
+                    onClick={onClose}
+                    className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 "
+                >
+                    닫기
+                </button>
+            </div>
         </Modal>
     );
 };
