da7a5773e40df7f2df0cc1ea527dd3be331f09ff
문지호 <moonwlgh@gmail.com>
2024-05-19 23:22:36 +0900

    Feat: 제료 삭제 alert

--- client/src/pages/management/Material/MaterialListInfo.jsx
+++ client/src/pages/management/Material/MaterialListInfo.jsx
@@ -3,13 +3,44 @@ import deleteIcon from "../../../images/orderlist/delete.svg";
 import { MaterialDeleteApi } from "../../../apis/server/MaterialApi";
 import { useState } from "react";
 import MaterialManagementModal from "./MaterialManagementModal";
+import Swal from "sweetalert2";
 
 const MaterialListInfo = ({ material, isLastItem }) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const deleteMaterial = MaterialDeleteApi(material.ingredientId);
-
     const handleDelete = () => {
-        deleteMaterial.mutate();
+        Swal.fire({
+            title: `"${material.ingredientName}"를 삭제하시겠습니까?`,
+            text: "이 작업은 되돌릴 수 없습니다!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3C50E0",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "삭제",
+            cancelButtonText: "취소",
+            reverseButtons: true,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteMaterial.mutate(null, {
+                    onSuccess: () => {
+                        Swal.fire({
+                            title: "삭제 완료!",
+                            text: "재료가 성공적으로 삭제되었습니다.",
+                            icon: "success",
+                            confirmButtonColor: "#3C50E0", // 버튼 색상 설정
+                            iconColor: "#3C50E0", // 아이콘 색상 설정
+                        });
+                    },
+                    onError: () => {
+                        Swal.fire(
+                            "오류 발생!",
+                            "재료를 삭제하는 도중 오류가 발생했습니다.",
+                            "error",
+                        );
+                    },
+                });
+            }
+        });
     };
     // 원화 포맷 함수
     const formatPrice = (price) => {
